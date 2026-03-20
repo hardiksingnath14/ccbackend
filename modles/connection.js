d1342acead1fe9958d2368d2b0fb176fd90ea0d4
Hardik Singnath <hardiksingnath@gmail.com>
2026-03-20 10:59:45 +0530

    connection

--- modles/connection.js
+++ modles/connection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-// import dotenv from "dotenv";
- mongoose.connect("mongodb://127.0.0.1:27017/silverpawn");
-// dotenv.config(); // 🔥 load env file
+ import dotenv from "dotenv";
+ //mongoose.connect("mongodb://127.0.0.1:27017/silverpawn");
+ dotenv.config(); // 🔥 load env file
 
-// mongoose.connect(process.env.DB_URL)
-// .then(() => console.log("MongoDB Connected"))
-// .catch(err => console.log(err));
+ mongoose.connect(process.env.DB_URL)
+ .then(() => console.log("MongoDB Connected"))
+ .catch(err => console.log(err));
 
 console.log("Mongodb connected successfully....");
 
