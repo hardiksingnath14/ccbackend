b0d221c2e9d8ed138cea586c8e03b05ce5c8a292
Hardik Singnath <hardiksingnath@gmail.com>
2026-03-20 10:24:36 +0530

    basi

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import UserRouter from "./routes/user.router.js";
 import ProductRouter from "./routes/porduct.router.js";
 import CategoryRouter from "./routes/category.router.js";
@@ -10,24 +9,33 @@ import ForgetPassword from './mailer_api/fp.controller.js';
 import SubCategoryRouter from "./routes/subcategory.router.js";
 import PaymentRouter from "./routes/payment.router.js";
 import aiChatRoute from "./routes/aiChat.js";
+
 dotenv.config();
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(fileUpload());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/ai", aiChatRoute);
-app.use("/user",UserRouter);
-app.use("/product",ProductRouter);
-app.use("/category",CategoryRouter);
-app.use("/subcategory",SubCategoryRouter);
+app.use("/user", UserRouter);
+app.use("/product", ProductRouter);
+app.use("/category", CategoryRouter);
+app.use("/subcategory", SubCategoryRouter);
 app.use("/payment", PaymentRouter);
 
-app.post("/forgetpassword",ForgetPassword);
+app.post("/forgetpassword", ForgetPassword);
+
 app.get("/", (req, res) => {
   res.send("✅ Server is live");
 });
-//route for forgetpassword
+
 if (process.env.NODE_ENV !== "production") {
   app.listen(3001, () => {
     console.log("Server running on http://localhost:3001");
